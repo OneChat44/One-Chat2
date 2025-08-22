75a8868c89c61b37da61e81d25c498dc41c1b54f
OneChat44 <prajayhiwale557@gmail.com>
2025-08-22 13:17:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,73 +1,29 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-
-let waitingUser = null;
+app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-    let nickname = '';
+    console.log('A user connected');
 
-    socket.on('login', (name) => {
-        nickname = name || 'Guest';
-        if (waitingUser) {
-            const partner = waitingUser;
-            waitingUser = null;
-
-            socket.partnerId = partner.id;
-            partner.partnerId = socket.id;
-
-            socket.emit('chatStart', partner.nickname);
-            partner.emit('chatStart', nickname);
-        } else {
-            waitingUser = socket;
-            socket.emit('waiting');
-        }
+    socket.on('login', (data) => {
+        console.log('Login:', data);
+        socket.emit('chatStart', 'SimulatedPartner'); // demo
     });
 
     socket.on('message', (msg) => {
-        if (socket.partnerId) io.to(socket.partnerId).emit('message', msg);
+        socket.emit('message', Partner: Echo -> ${msg});
     });
 
     socket.on('skip', () => {
-        const oldPartnerId = socket.partnerId;
-        if (oldPartnerId) {
-            io.to(oldPartnerId).emit('partnerLeft');
-            io.sockets.sockets.get(oldPartnerId).partnerId = null;
-        }
-        socket.partnerId = null;
-        if (waitingUser && waitingUser.id !== socket.id) {
-            const partner = waitingUser;
-            waitingUser = null;
-
-            socket.partnerId = partner.id;
-            partner.partnerId = socket.id;
-
-            socket.emit('chatStart', partner.nickname);
-            partner.emit('chatStart', nickname);
-        } else {
-            waitingUser = socket;
-            socket.emit('waiting');
-        }
-    });
-
-    socket.on('disconnect', () => {
-        if (waitingUser && waitingUser.id === socket.id) waitingUser = null;
-        if (socket.partnerId) {
-            io.to(socket.partnerId).emit('partnerLeft');
-            io.sockets.sockets.get(socket.partnerId).partnerId = null;
-        }
+        socket.emit('partnerLeft');
+        setTimeout(() => socket.emit('chatStart', 'NewPartner'), 500);
     });
 });
 
-
-server.listen(PORT, () => console.log('Server running on port ${PORT}'));
+server.listen(3000, () => console.log('Server running on port 3000'));
